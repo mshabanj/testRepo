96d52495dae4c14ed2bc3dfc013d67564064b6b9
mshabanj <mjok002@aucklanduni.ac.nz>
2015-11-13 21:06:23 +1300

    added all files by myself

---
+++ src/com/demo/examples/DemoServlet.java
@@ -0,0 +1,54 @@
+package com.demo.examples;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DemoServlet
+ */
+@WebServlet(description = "my demo Example Servlet", urlPatterns = { "/DemoServlet" })
+public class DemoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DemoServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter w= response.getWriter();
+		w.println("<!DOCTYPE html>"
+				+ "<html>"
+				+ "<head>"
+					+ "<title>My first Demo Servlet...</title>"
+				+ "</head>"
+				+ "<body>"
+					+ "<h1> Demo Servlet.. </h1>"
+					+ "<b> Something to describe.. </b>"
+				+ "</body"
+				+ "</html>");		
+		//w.println("test servlet...");
+		w.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
